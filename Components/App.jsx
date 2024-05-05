8d1b80038b888dfd168e9ae632cbbceafa492791
Dániel Beck <danny252252@gmail.com>
2024-05-05 16:20:03 +0200

    new browserrouter

--- Components/App.jsx
+++ Components/App.jsx
@@ -1,34 +1,29 @@
 import React from "react";
 import Header from "./Header";
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Skills from "./Skills";
 import WrokProject from "./WorkProjects";
-import About from "./About";  
+import About from "./About";
 import Resume from "./Resume";
 import Contact from "./Contact";
-
 
 function App() {
   return (
     <div>
-
-      <Header />
+      <BrowserRouter
+        basename={import.meta.env.DEV ? "/" : "/react-vite-gh-pages/"}
+      >
+        <Header />
         <Routes>
-          <Route path="/Home" element={<Home />}>
-          </Route>
-          <Route path="/Skills" element={<Skills />}>
-          </Route>
-          <Route path="/WorkProjects" element={<WrokProject />}>
-          </Route>
-          <Route path="/About" element={<About />}> 
-          </Route>
-          <Route path="/Resume" element={<Resume />}>
-          </Route>
-          <Route path="/Contact" element={<Contact />}>
-          </Route>
+          <Route path="/Home" element={<Home />}></Route>
+          <Route path="/Skills" element={<Skills />}></Route>
+          <Route path="/WorkProjects" element={<WrokProject />}></Route>
+          <Route path="/About" element={<About />}></Route>
+          <Route path="/Resume" element={<Resume />}></Route>
+          <Route path="/Contact" element={<Contact />}></Route>
         </Routes>
-        
+      </BrowserRouter>
     </div>
   );
 }
